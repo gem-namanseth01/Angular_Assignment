575397c27279313c85236c39b8ed5745d1733f4a
gem-namanseth01 <naman.seth@geminisolutions.com>
2022-05-09 21:15:25 +0530

    completed login component

--- src/app/reactive-driven-form/log-in/log-in.component.ts
+++ src/app/reactive-driven-form/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-log-in',
@@ -7,7 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+  arr = JSON.parse(localStorage.getItem('arr1') || '[]');
+
+  login = this.fb.group({
+    userName: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(8)]]
+  })
+  flag :boolean = false;
+
+  one() {
+    for (var product of this.arr) {
+      if(this.login.value.userName==product.name && this.login.value.password==product.password){
+        this.flag = true;
+        return
+      }
+    }
+    alert("username or password is invalid!!")
+  }
+
 
   ngOnInit(): void {
   }
